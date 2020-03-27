3280a697d773c6d8494aadd5cd0aad800457892e
Churches <32278121+HugoChurches@users.noreply.github.com>
2020-03-27 08:26:49 -0600

    Add files via upload

---
+++ Coding-Challenge-3/js/index.js
@@ -0,0 +1,14 @@
+let value = '';
+
+
+value = document.getElementsByClassName('fizzBuzzNumber')[0].value;
+
+
+document.getElementById("submitBtn").addEventListener("click", checkVal);
+
+
+function checkVal()
+{
+    console.log("OK");
+}
+console.log(value);
